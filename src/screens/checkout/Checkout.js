cfd56d5de6922bf099ebe137881d86f6e27523d2
Soumya Ranjan Tripathy <sibun.soumya@gmail.com>
2021-06-13 19:24:24 +0530

    added existing address tab

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -1,13 +1,66 @@
 
-import React from "react";
+import React, { Component,Fragment } from "react";
+import "./Checkout.css";
+import {Redirect} from 'react-router-dom';
+import { StepLabel,Stepper,Step,StepContent,AppBar, Tabs, Tab } from "@material-ui/core";
+class Checkout extends Component {
+  constructor() {
+    super();
+    this.state = {
+        activeStep: 0,
+        activeTabValue: 'existing_address',
+        addresses: [],
+        states: [],
+        payments: [],
+        flat: '',
+        locality: '',
+        city: '',
+        stateUUID: '',
+        pincode: '',
+        paymentId: '',
+        flatRequired: false,
+        localityRequired: false,
+        cityRequired: false,
+        stateUUIDRequired: false,
+        pincodeRequired: false,
+        pincodeValid: true,
+        selectedAddressId: undefined,
+        displayChange: 'display-none',
+        placeOrderMessage: undefined,
+        placeOrderMessageOpen: false,
+        couponId: undefined,
+    }
+}
 
-const Checkout = props => {
-  
-  return (
-    <>
-      <p>This is checkout page</p>
-    </>
-  );
-};
+
+  render() {
+
+    
+
+    return <Fragment>
+      <div className = "main-container">
+        <div className="delivery-payment-section">
+          <Stepper activeStep = {this.state.activeStep} orientation='vertical'>
+            <Step key='Delivery'>
+              <StepLabel>Delivery</StepLabel>
+              <StepContent>
+                <div>
+                  <AppBar position={"relative"}>
+                    <Tabs value={this.state.activeTabValue} variant='standard'>
+                      <Tab value='existing_address' label='EXISTING ADDRESS'
+                      onClick={() => this.changeActiveTab('existing_address')}/>
+                    </Tabs>
+                  </AppBar>
+                </div>
+              </StepContent>
+            </Step>
+          </Stepper>
+        </div>
+      </div>
+    </Fragment>
+  }
+}
+
+
 
 export default Checkout;
